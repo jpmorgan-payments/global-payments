362fb7f8a310b901d7cda37b3ceda206be0e9cac
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2023-02-20 10:56:16 +0000

    Java demo

--- digital-signature-creation/java/src/main/java/com/jpmorgan/payments/digital_signature/JWTDemo.java
+++ digital-signature-creation/java/src/main/java/com/jpmorgan/payments/digital_signature/JWTDemo.java
@@ -2,30 +2,31 @@ package com.jpmorgan.payments.digital_signature;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import javax.xml.bind.DatatypeConverter;
+import org.json.JSONObject;
 
 public class JWTDemo
 
 {
-    // Your digital signature key, this should not be stored like this in your
-    // system.
-    private static String SECRET_KEY = "";
 
-    // The JSON payload to be digitally signed.
-    private static String PAYLOAD = "";
+    /**
+     * Method for reading in a private key and preparing it for signing
+     * 
+     * @param secretKey Your digital signature key that has been configured in
+     *                  developer.jpmorgan.com
+     * @return a Private Key object
+     */
+    private static PrivateKey gatherPrivateKey(String secretKey) {
 
-    public static void main(String[] args) throws InvalidKeySpecException, NoSuchAlgorithmException {
-        System.out.println(createJWT());
-    }
+        secretKey = secretKey.replace("-----BEGIN PRIVATE KEY-----", "");
+        secretKey = secretKey.replace("-----END PRIVATE KEY-----", "");
 
-    private static PrivateKey gatherPrivateKey() {
-        byte[] prvKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        byte[] prvKeyBytes = DatatypeConverter.parseBase64Binary(secretKey);
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(prvKeyBytes);
         PrivateKey prvKey = null;
         try {
@@ -41,15 +42,24 @@ public class JWTDemo
         return prvKey;
     }
 
-    public static String createJWT() throws InvalidKeySpecException, NoSuchAlgorithmException {
+    /**
+     * Method for signing a JSON payload
+     * 
+     * @param payload   The JSON payload to be digitally signed.
+     * @param secretKey Your digital signature key that has been configured in
+     *                  developer.jpmorgan.com
+     * @return
+     */
+    public static String createJWT(String payload, String secretKey) {
 
         // The JWT signature algorithm we will be using to sign the token
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.RS256;
 
         // Prepare your private key for signing
-        PrivateKey prvKey = gatherPrivateKey();
+        PrivateKey prvKey = gatherPrivateKey(secretKey);
+        final JSONObject obj = new JSONObject(payload);
 
-        return Jwts.builder().claim("payments", PAYLOAD)
+        return Jwts.builder().setClaims(obj.toMap())
                 .signWith(prvKey, signatureAlgorithm).compact();
 
     }
