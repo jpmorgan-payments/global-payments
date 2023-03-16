1e88b47526790d3ed85bd304b1a7d9d53c9b95ae
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2023-03-16 13:24:37 +0000

    Update code

--- digital-signature-creation/go/digital_signature.go
+++ digital-signature-creation/go/digital_signature.go
@@ -6,13 +6,11 @@ import (
 )
 
 func generate_digital_signature(digital_key string, body map[string]interface{}) string {
-    fmt.Println("hello")
     token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims(body))
 
     signKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(digital_key))
 
     tokenString, err := token.SignedString(signKey)
-    fmt.Println("hey")
 
     if err != nil {
         fmt.Println(err)
