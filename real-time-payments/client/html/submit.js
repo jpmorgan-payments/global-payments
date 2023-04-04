17c914a787ec40a2d5c5a3320eef40e9932fe82d
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2023-04-04 11:26:35 +0100

    Fixing HTML for now

---
+++ real-time-payments/client/html/submit.js
@@ -0,0 +1,20 @@
+function sendPaymentClicked() {
+    const formElement = document.querySelector("form");
+    const formData = new FormData(formElement);
+    var object = {};
+    formData.forEach((value, key) => object[key] = value);
+    var json = JSON.stringify(object);
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        console.log(request.readyState);
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 202 || request.status === 200) {
+          window.location.href = "success.html";
+        }else{
+            window.location.href = "error.html";
+        }
+      }
+    };
+    request.open("POST", "/digitalSignature/tsapi/v1/payments");
+    request.send(json);
+  }
